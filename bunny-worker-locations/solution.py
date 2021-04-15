4f109b7f6733b4a3d3053121d8c53eae178a6563
Jaw-Ahm <BlckN25^^>
2021-04-15 13:23:45 -0500

    level 2 part 2

--- bunny-worker-locations/solution.py
+++ bunny-worker-locations/solution.py
@@ -20,27 +20,6 @@ def main():
     else:
         print("Fail!", output)
 
-    # # Test case 3
-    # output = solution([4, 3, 5, 7, 8], 12)
-    # if output == [0,2]:
-    #     print("Pass!", output)
-    # else:
-    #     print("Fail!", output)
-    #
-    # # Test case 3
-    # output = solution([1, 4, 20, 3, 10, 5], 33)
-    # if output == [2, 4]:
-    #     print("Pass!", output)
-    # else:
-    #     print("Fail!", output)
-    #
-    # # Test case 5
-    # output = solution([1, 4, 0, 0, 3, 10, 5], 1)
-    # if output == [0,0]:
-    #     print("Pass!", output)
-    # else:
-    #     print("Fail!", output)
-
 
 if __name__ == "__main__":
     main()
